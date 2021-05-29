ea9445f329e2ffd33ff9effe642187665e503e39
Felippe Correa <felippecorrea2010@gmail.com>
2021-05-29 15:11:06 -0300

    ADicionando exercícos do bloco 13

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import About from './components/About';
+import Users from './components/Users';
+import StrictAccess from './StrictAccess';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/users">Users</Link></li>
+              <li><Link to="/strict-access">Strict Access</Link></li>
+            </ul>
+          </nav>
+        <Switch>
+          <Route exact path="/Users/:id" render={(props) => <Users {...props} greetingMessage={'Good MOrning'}/>} />
+          <Route path="/strict-access" render={ () => ( <StrictAccess user={ { username: 'joao', password: '1234' } } />)} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/" component={Home} />
+       </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Users extends Component {
+class Users extends React.Component {
   render() {
+    const { greetingMessage } = this.props;
+    const { id } = this.props.match.params;
     return (
       <div>
-        <h2>Users</h2>
-        <p> My awesome Users component </p>
+        <h2> Users </h2>
+        <p> {greetingMessage}, this is my awesome Users {id} component </p>
       </div>
-    );
+    )
   }
-};
+}
 
 export default Users;
